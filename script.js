1e39fdb6ab69f65aa77e97334c34f429653d8480
Sudheeksha02 <sudheekshavydadi@gmail.com>
2026-06-14 15:06:37 +0530

    Portfolio updated

---
+++ script.js
@@ -0,0 +1,37 @@
+const text = "Python Enthusiast | Web Developer";
+
+let index = 0;
+
+function typeEffect() {
+
+    document.getElementById("typing")
+    .innerHTML = text.slice(0, index);
+
+    index++;
+
+    if (index <= text.length) {
+        setTimeout(typeEffect, 100);
+    }
+}
+
+typeEffect();
+
+function darkMode() {
+    document.body.classList.toggle("dark");
+}
+
+window.onscroll = function () {
+
+    let scrollTop =
+        document.documentElement.scrollTop;
+
+    let height =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+    let scrolled =
+        (scrollTop / height) * 100;
+
+    document.getElementById("progress-bar")
+        .style.width = scrolled + "%";
+};
